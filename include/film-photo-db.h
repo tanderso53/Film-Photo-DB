b08bd469b7d505b848f24105a8237e4d6b25533c
Tyler J. Anderson <tasonchou@gmail.com>
2022-08-21 23:38:09 -0500

    Added meta-header for film photo db C and C++ interface

---
+++ include/film-photo-db.h
@@ -0,0 +1,27 @@
+/**
+ * @file film-photo-db.h
+ * @author Tyler J. Anderson
+ * @copyright (2022) TYler J. Anderson use under BSD 3-clause license
+ * @brief Meta-header for the C and C++ APIs for the film photo DB
+ */
+
+#ifndef FILM_PHOTO_DB_H
+#define FILM_PHOTO_DB_H
+
+#ifdef __cplusplus
+
+#include "db-model.hxx"
+
+extern "C" {
+
+#endif /* #ifdef __cplusplus */
+
+#include "db-interface.h"
+
+#ifdef __cplusplus
+
+}
+
+#endif /* #ifdef __cplusplus */
+
+#endif /* #ifndef FILM_PHOTO_DB_H */
